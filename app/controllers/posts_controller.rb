e45463638cfdf7d0ce2b2d12883161b2c20905d7
ivnicolas <ikennanicolas@gmail.com>
2021-04-27 21:20:09 -0400

    commit tree 5e80bdf2529b4c5c16a6bcc3e057eb78e9cdf52c

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,17 +3,30 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def new 
+  end 
+
+
   def edit
     @post = Post.find(params[:id])
   end
 
+  def create 
+  end 
+
+
   def update
     @post = Post.find(params[:id])
-
     @post.update(post_params)
+    if @post.valid?
+      @post.save
+      redirect_to post_path(@post)
+    else 
+      render :edit
+    end 
 
-    redirect_to post_path(@post)
   end
+
 
   private
 
